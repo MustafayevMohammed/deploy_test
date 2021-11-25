c8a91fee293391ec592ac21ecacad7927f15d94d
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2021-11-25 11:55:48 +0400

    version-1.5

--- anywhere_test/settings.py
+++ anywhere_test/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'anywhere_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates"],
+        'DIRS': ["aywhere_test/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
