936f3b05d5a6b4fba86187fcc485c7d7f174e993
Mehemmed Mustafayev <mohammedmustafayev@gmail.com>
2021-11-25 11:45:55 +0400

    version-1.3

--- anywhere_test/settings.py
+++ anywhere_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-14zvcqw@=z2oh%1&12-=l3z6xxqk4elt9l-fm6qq$y6mmp=!$n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mehemmed.pythonanywhere.com"]
 
 
 # Application definition
